a93bc78493a1e2c2d76c9e1d0d67cffa5c79af60
pdrocosta <pedro.augusto.costa.99@gmail.comgit>
2023-01-05 16:45:31 -0300

    index

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,55 @@
-/* Monte sua lógica aqui */
+// Definimos essa função como assíncrona pois precisamos aguardar a resposta da função consomePokeAPI
+async function renderizaPokemons() {
+  const ulTag = document.querySelector('ul')
+
+  // Aqui utilizaremos o await para aguardar a resposta da função
+  const listaDePokemons = await consomePokeAPI()
+
+  // Agora é só iterar sob o array e renderizar cada pokemon
+  listaDePokemons.results.forEach(pokemon => {
+      // "Fatia" o número do pokemon na pokedex a partir da URL de cada pokemon
+      const numeroNaPokedex = pokemon.url.slice(34, -1)
+
+      ulTag.insertAdjacentHTML('beforeend', `
+          <li>
+              <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${numeroNaPokedex}.png" alt=${pokemon.name}>
+              <h3>${pokemon.name}</h3>
+          </li>
+      `)
+  })
+}
+
+// Chama a função para rodá-la ao carregar a página
+renderizaPokemons()
+
+console.log(renderizaPokemons())
+
+function renderPokemon(array){
+  const ul = document.querySelector('ul')
+
+  ul.innerHTML = ''
+
+  array.forEach(element => {
+    const card = createCard(element)
+
+    ul.append(card)
+  })
+}
+
+function createCard({img, name}) {
+  const container = document.createElement('li')
+  const image = document.createElement('img')
+  const p = document.createElement('p')
+
+  image.src = img
+  image.alt = name
+
+  h2.innerText = name
+
+  p.innerText = level
+
+  container.append(image, p)
+
+  return container
+}
+
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,1 +1,54 @@
 /* Monte a lógica das requisições aqui */
+// requests.js
+
+async function consomePokeAPI() {
+    // Seleciona o elemento que representa o loading da requisição
+    const loading = document.querySelector('#loading')
+
+    // Faz a requisição na API
+    const pokemonsDaAPI = await fetch('https://pokeapi.co/api/v2/pokemon')
+      .then(
+        /*  Converte o retorno para um objeto Javascript válido */
+        response => response.json()
+      )
+      .catch(
+        /* Caso haja algum erro, retornamos ele no console */
+        error => console.log(error)
+      )
+
+    // Independente da requisição ser um sucesso, ou um erro, removeremos o loading da tela
+    loading.classList.add('hidden')
+
+    // Retorna esse valor convertido
+    return pokemonsDaAPI
+}
+
+// Chama a função para rodá-la ao carregar a página
+consomePokeAPI()
+
+async function getPokemonByName(pokemonName) {
+    const pokemon = await fetch(`'https://pokeapi.co/api/v2/pokemon/'${pokemonName}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(res => res.json()) 
+    .then(res => {
+        
+        return res
+    })
+    renderPokemon(pokemon)
+    return pokemon
+  }
+  
+  function renderSearch() {
+    const searchInput = document.querySelector('input')
+    const searchBtn = document.querySelector('#searchBtn')
+  
+    searchBtn.addEventListener('click', () => {
+      getPokemonByName(searchInput.value.toLowerCase().trim())
+    })
+  }
+  
+  renderSearch()
